808c58173979955a915c8a2da452360cc77471df
antong <antong@80da951e-da95-11de-849f-b362a63f070a>
2009-11-28 19:30:35 +0000

    Number of refactorings

--- src/helpers/db.py
+++ src/helpers/db.py
@@ -4,21 +4,51 @@ Created on Nov 24, 2009
 @author: Anton Gorshkov
 '''
 from google.appengine.ext import db
-
-class Event(db.Model):
-    borough = db.StringProperty(required=False)
-    eventDescription = db.StringProperty(required=False)
-    eventEndDate = db.StringProperty(required=False)
-    eventName = db.StringProperty(required=False)
-    eventStartDate = db.StringProperty(required=False)
-    eventTypeDescription = db.StringProperty(required=False)
-    eventid = db.IntegerProperty()
-    location = db.StringProperty(required=False)
+from geopy import geocoders
+from geopy import distance
+from geopy import Point
+from helpers import logger
+from models import Event
     
 def query(search):
-    query = Event.all()
-    results = [];
+    mile = 0.01502 #bad bad approximation - need to calculate more precise
+    range = mile*0.1 #by default 5 mile 'radius', in future need to give option
+    g = geocoders.Google('ABQIAAAAAnMK37-crb-IVXX2SNmBOhStP4HpWo52j4u-OwfYEqnsxFY73BSpaiVrjhMtwbsCCfu2NkyPhj6myA')  
+    place, (lat, lng) = g.geocode(search)
+    lng_min = lng - range
+    lng_max = lng + range
+    lat_min = lat - range
+    lat_max = lat + range
+    logger.LogIt("lng_min: " + str(lng_min))
+    logger.LogIt("lng_min: " + str(lng_max))   
+    logger.LogIt("lat_min: " + str(lat_min))
+    logger.LogIt("lat_min: " + str(lat_max))
+    
+#    Can't query by two parameters, so only querying on one and then doing distance calculation
+#    This is not good.  Need GeoHash based algorithm instead.
+    query = db.GqlQuery("SELECT * FROM Event WHERE longtitude > :lomin AND longtitude < :lomax", 
+                        lomin=lng_min, lomax=lng_max )
+#    query = db.GqlQuery("SELECT * FROM Event WHERE longtitude > :lomin AND longtitude < :lomax"
+#                    "AND latitude > :lamin AND latitude < :lamax",
+#                    lomin=lng_min, lomax=lng_max, lamin=lat_min, lamax=lat_max)
+     
+    ret_results = []
     for result in query:
-        if search.lower() in result.location.lower():
-            results.append( result )
-    return( results )
+        p1 = Point(result.latitude, result.longtitude)
+        p2 = Point(lat, lng) 
+        result.distance = distance.distance(p1, p2).miles
+        ret_results.append( result )
+    
+    def compare(a, b):
+        return cmp(a.distance, b.distance)
+    
+    ret_results.sort(compare)
+       
+    return( ret_results[0:5] ) 
+        
+    #1. Build a box of 5 mile radius
+    #2. Search within the box
+    #3. Iterate over results, calculating distance of each one
+    #4. Sort by Distance and return first 3
+    
+#    query = Event.all()
--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ from google.appengine.api import mail
 from helpers import logger
 from helpers import parser
 from helpers import db
+from geopy import geocoders
+#from urllib import urlencode
+from urllib2 import urlopen
 
 class MailHandler(InboundMailHandler):
     def receive(self, mail_message):
@@ -24,21 +27,33 @@ class MailHandler(InboundMailHandler):
         logger.LogIt("Body is %s" % txtmsg)
         logger.LogIt("Length of body is: " + str(len(txtmsg)))        
         
-        message = mail.EmailMessage()
-        message.sender = "daveou@gmail.com"
-        message.to = mail_message.sender
-        message.subject = "Re: " + mail_message.subject
+        sender = "daveou@gmail.com"        
+        subject = "Re: " + mail_message.subject
 #        message.body = "Got your message" + txtmsg
         body = "Results:\n"
         #txtQuery = parser.ParseIt(txtmsg)
         txtQuery = str(txtmsg)        
         results = db.query(txtQuery)
         i=1
+        #REMOVE FROM HERE! DRY!!!!!
+        api_key = "ABQIAAAAAnMK37-crb-IVXX2SNmBOhStP4HpWo52j4u-OwfYEqnsxFY73BSpaiVrjhMtwbsCCfu2NkyPhj6myA"
+        g = geocoders.Google(api_key)  
+        place, (lat, lng) = g.geocode(txtQuery)    
+        markers = "&markers=color:red|label:You|"+str(lat)+","+str(lng)
         for res in results:
             body = body + "(" + str(i) + ") " + res.eventName + ": " + res.eventDescription + " @ " + res.location
-            i=i+1 
-        message.body = body
-        message.send()
+            markers = markers + "&markers=color:blue|label:"+str(i)+"|"+str(res.latitude)+","+str(res.longtitude) 
+            i=i+1
+        url="http://maps.google.com/maps/api/staticmap?center="+str(lat)+","+str(lng)
+        url=url+"&zoom=14&size=512x512&maptype=roadmap&sensor=false"+markers+"&key="+api_key
+        logger.LogIt("URL is: " + url)
+        filehandle = urlopen(url)              
+        mail.send_mail(sender=sender,
+                       to=mail_message.sender,
+                       subject=subject,
+                       body=body,
+                       attachments=[("pic.png", filehandle.read())]
+                       )
          
         
 class MainPage(webapp.RequestHandler):
@@ -59,16 +74,30 @@ class WebSMS(webapp.RequestHandler):
         results = db.query(txtQuery)
         i=1
         for res in results:
-            self.out("(" + str(i) + ") " + res.eventName + ": " + res.eventDescription + " @ " + res.location + "<br>")
+            self.out("(" + str(i) + ") " + res.eventName + ": " + res.eventDescription + " @ " + res.location + "(" + str(res.distance) + " miles)<br>")
             i=i+1
         self.out("</div></div></body></html>")        
                 
     def out(self, txt):
         self.response.out.write(txt)
         
+class QuickClass(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("Hello There2!<br>")
+        logger.LogIt("Q get called" )
+        url = "http://maps.google.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=14&size=512x512&maptype=roadmap\&markers=color:blue|label:S|40.702147,-74.015794&markers=color:green|label:G|40.711614,-74.012318&markers=color:red|label:C|40.718217,-73.998284&sensor=false&key=ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSsTL4WIgxhMZ0ZK_kHjwHeQuOD4xQJpBVbSrqNn69S6DOTv203MQ5ufA"
+        filehandle = urlopen(url)              
+        mail.send_mail(sender="daveou@gmail.com",
+                       to="6467338252@mms.att.net",
+                       subject="Test Sending Google Image",
+                       body=";)",
+                       attachments=[("pic.png", filehandle.read())]
+                       )
+            
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
                                       ('/websms', WebSMS),
+                                      ('/q', QuickClass),                                      
                                       MailHandler.mapping()
                                       ],                                      
                                      debug=True)
