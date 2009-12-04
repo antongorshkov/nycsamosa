e8a1f567c0956cbbf851f85528e8dcd8e6255fac
antong <antong@80da951e-da95-11de-849f-b362a63f070a>
2009-12-04 16:07:03 +0000

    Added additional models + description methods

--- trunk/src/models/models.py
+++ trunk/src/models/models.py
@@ -1,20 +1,92 @@
 from google.appengine.ext import db
 
+class GenericResult():
+    description = ""
+    def description(self):
+        return self.description
+    
 class Event(db.Model):
-    eventid = db.IntegerProperty()    
-    eventName = db.StringProperty()    
+    eventid = db.IntegerProperty()   
+    eventName = db.StringProperty()   
     eventTypeDescription = db.StringProperty()
     eventDescription = db.StringProperty()
-    eventStartDate = db.StringProperty()    
-    eventEndDate = db.StringProperty()        
-    location = db.StringProperty()    
+    eventStartDate = db.StringProperty()   
+    eventEndDate = db.StringProperty()       
+    location = db.StringProperty()   
     borough = db.StringProperty()
     longtitude = db.FloatProperty()
     latitude = db.FloatProperty()
-        
+    distance = db.FloatProperty()
+    def description(self):
+        return self.eventName + ": " + self.eventDescription + " @ " + self.location + "(" + str(self.distance) + " miles)"
+    def long(self):
+        return "longtitude"
+       
 class Street(db.Model):
-    street = db.StringProperty()    
-    zipcodes = db.IntegerProperty()
-    borough = db.StringProperty()
+    street = db.StringProperty()   
+    zipcode = db.IntegerProperty()
+    borough = db.StringProperty()   
 
+class Parking(db.Model):
+    facilityType = db.StringProperty()
+    licenseNumber = db.StringProperty()
+    statusEntityLicense = db.StringProperty()
+    licenseExpirationDate = db.StringProperty()
+    entityName = db.StringProperty()
+    tradeName = db.StringProperty()
+    addressBldg = db.StringProperty()
+    addressStreetName = db.StringProperty()
+    akaAddress = db.StringProperty()
+    addressCity = db.StringProperty()
+    addressState = db.StringProperty()
+    addressZipCode = db.IntegerProperty()
+    telephoneNumber = db.IntegerProperty()
+    numberofSpaces = db.IntegerProperty()
+    webAddress = db.StringProperty()
+    latitude = db.FloatProperty()
+    longtitude = db.FloatProperty()
+    distance = db.FloatProperty()
+    
+    def description(self):
+        return self.entityName + "@" + self.webAddress
+    
+    def long(self):
+        return "longtitude" 
 
+class AltParking(db.Model):
+    reason = db.StringProperty()
+    date = db.DateTimeProperty()
+   
+class Laundromat(db.Model):
+    camisTradeName = db.StringProperty()
+    entityName = db.StringProperty()
+    licenseNumber = db.IntegerProperty()
+    address = db.StringProperty()
+    longitude = db.FloatProperty()
+    latitude = db.FloatProperty()
+    distance = db.FloatProperty()
+    
+    def description(self):
+        return self.entityName + "@" + self.address
+    
+    def long(self):
+        return "longitude"    
+   
+class SidewalkCafe(db.Model):
+    licenseNumber = db.IntegerProperty()
+    name = db.StringProperty()
+    tradeTypeCamis = db.StringProperty()
+    address = db.StringProperty()
+    telephoneNumber = db.IntegerProperty()
+    squareFootage = db.IntegerProperty()
+    locationAddress = db.StringProperty()
+    licenseExpirationDate = db.DateTimeProperty()
+    latitude = db.FloatProperty()
+    longitude = db.FloatProperty()
+    distance = db.FloatProperty()
+    
+    def description(self):
+        return self.name + "@" + self.address
+    
+    def long(self):
+        return "longitude"    
